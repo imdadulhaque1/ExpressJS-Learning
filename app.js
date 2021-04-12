f568a340d0d441853e331ce45d79d5c396fdd8a3
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-04-13 01:01:27 +0600

    Using Postman(Google extension) for GET Request

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
  // Express
  const express = require('express');
+ const fs= require('fs');
  const app = express();
 
  app.get('/', (request, response) =>{
-           response.send("Hello from express js !");
+          response.send("Hello from express js !");
  });
 
- app.get('/another', (request, response) =>{
-          response.send("I am another response !");
-});
-
- app.get('/students', (request, response) =>{
-          response.send(["Imdadul Haque", "Israt Jahan Maisha"]);
-});
+ app.get('/api/students', (request, response) => {
+          fs.readFile('./db.json', 'utf-8', (err, data) =>{
+                    const students= JSON.parse(data).students;
+                    response.send(students);
+          });
+ });
 
  const port = 3000;
 //  const port = 3001;
 
  app.listen(port, () =>{
-           console.log(`Listening on port 3000..........`);
+          console.log(`Listening on port 3000..........`);
  })
